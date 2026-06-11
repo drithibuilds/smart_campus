a269329603c5f7053e496eccb10dad7c4f2a8074
drithibuilds <drithiraj164@gmail.com>
2026-06-12 01:54:25 +0530

    Add SSL support for TiDB

--- config/db.js
+++ config/db.js
@@ -2,11 +2,17 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'campus_ccms',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: false
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
